b12f8e654559d8479c61bd1a7f56c449a88e4570
Thioalejo <alejo.saraza@gmail.com>
2024-09-01 15:04:46 -0500

    Se implementa mejoras para que solo se permitan movimientos  entre piezas del lado y no cualquiera

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -102,7 +102,7 @@ public class Board : MonoBehaviour
     //Cuando levanto el clic
     public void TileUp(Tile tile_)
     {
-        if(startTile!=null && endTile != null)
+        if(startTile!=null && endTile != null && IsCloseTo(startTile, endTile))
         {
             SwapTiles();
         }
@@ -125,5 +125,20 @@ public class Board : MonoBehaviour
         Pieces[startTile.x, startTile.y] = EndPiece;
         Pieces[endTile.x, endTile.y] = StartPiece;
 
+    }
+
+    public bool IsCloseTo(Tile start, Tile end)
+    {
+        //Validamos movimiento horizontal
+        if(Math.Abs((start.x - end.x))==1 && start.y == end.y){
+            return true;
+        }
+
+        if (Math.Abs((start.y - end.y)) == 1 && start.x == end.x)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
