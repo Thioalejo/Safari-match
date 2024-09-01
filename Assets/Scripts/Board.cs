0730df00280accbec7d2c4f5947163026445936b
Thioalejo <alejo.saraza@gmail.com>
2024-09-01 13:07:45 -0500

    Se  implementan ajustes para que la camara se  posicione de manera correcta segun la cuadricula

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -9,10 +9,31 @@ public class Board : MonoBehaviour
     public int height;
     public GameObject tileObject;
 
+    //PAra personalizar la posision y ortografico de la camara
+    public float cameraSizeOffset;
+    public float cameraVerticalOffset;
+
     // Start is called before the first frame update
     void Start()
     {
         SetupBoard();
+        PositionCamera();
+    }
+
+    private void PositionCamera()
+    {
+        float newPosX = (float)width / 2f;
+        float newPosY = (float)height / 2f;
+
+        Camera.main.transform.position = new Vector3(newPosX-0.5f, newPosY - 0.5f + cameraVerticalOffset, -10f);
+
+        // se obtiene el tamaño ortografico de la camara para ajustar la visual segun la cantidad de elementos creados de la matriz
+
+        float horizontal = width + 1;
+        float vertical = (height/2 ) + 1;
+
+        Camera.main.orthographicSize = horizontal > vertical ? horizontal +cameraSizeOffset: vertical+cameraSizeOffset;
+
     }
 
     private void SetupBoard()
